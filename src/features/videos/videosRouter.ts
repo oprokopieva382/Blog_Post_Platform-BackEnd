8216e610e2ee3814907468e41e6ff54e68f6013c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-03-29 18:49:38 -0500

    clean up commented code

--- src/features/videos/videosRouter.ts
+++ src/features/videos/videosRouter.ts
@@ -1,19 +1,8 @@
 import Router from "express";
-import { getVideosController } from "./controllers/getVideosController";
-import { createVideoController } from "./controllers/createVideoController";
-import { getByIdVideoController } from "./controllers/getByIdVideoController";
-import { updateVideoController } from "./controllers/updateVideoController";
-import { deleteVideoController } from "./controllers/deleteVideoController";
-import { videosController } from './videosController';
 import { db } from "../../db/db";
+import { videosController } from './videosController';
 
 export const videosRouter = Router();
-
-//videosRouter.get("/", getVideosController)
-//videosRouter.get("/:id", getByIdVideoController);
-//videosRouter.post("/", createVideoController);
-//videosRouter.put("/:id", updateVideoController);
-//videosRouter.delete("/:id", deleteVideoController);
 
 videosRouter.get("/", videosController.getAll(db));
 videosRouter.get("/:id", videosController.getById(db));
