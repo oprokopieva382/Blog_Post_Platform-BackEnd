2047024d3b116f7ff549081bc66a75131efb03d5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-03-29 20:24:45 -0500

    update app.ts to take care of data clean up

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { SETTINGS } from "./settings";
 import { videosRouter } from "./features/videos/videosRouter";
-import { db } from "./db/db";
+import { testingRouter } from "./features/dbCleanUp/testingRouter";
 
 export const app = express();
 
 //use middleware to be able have access to body and query of all needed requests
 app.use(express.json());
+app.use(SETTINGS.PATH.TESTING, testingRouter);
 app.use(SETTINGS.PATH.VIDEOS, videosRouter);
 
 app.get("/", (req, res) => {
