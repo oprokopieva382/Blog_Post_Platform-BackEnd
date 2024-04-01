b6ba90186c0f4ce6842dc06d02c1600b755db682
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-01 13:59:11 -0500

    add PostInputModel and BlogViewModel

---
+++ src/features/posts/models/PostInputModel.ts
@@ -0,0 +1,12 @@
+export type PostInputModel = {
+  /**
+   * Post title (required field & maxLength: 30)
+   * Post shortDescription (required field & maxLength: 100)
+   * Post content (required field, maxLength: 1000)
+   * Post to blogId joint (required field)
+   */
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+};
