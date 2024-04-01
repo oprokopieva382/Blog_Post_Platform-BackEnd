42f58bdd079d5936051139149496ce02c60a34c0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-03-31 22:25:53 -0500

    modify code in validation for publicationDate

--- src/utils/updateValidation.ts
+++ src/utils/updateValidation.ts
@@ -16,12 +16,12 @@ export const updateValidation = (data: Video) => {
     });
   }
 
-   if (!data.title) {
-     errors.errorsMessages.push({
-       message: "title field is required",
-       field: "title",
-     });
-   }
+  if (!data.title) {
+    errors.errorsMessages.push({
+      message: "title field is required",
+      field: "title",
+    });
+  }
 
   if (data.title && data.title.length > 40) {
     errors.errorsMessages.push({
@@ -37,16 +37,16 @@ export const updateValidation = (data: Video) => {
     });
   }
 
-if (
-  data.minAgeRestriction !== null &&
-  typeof data.minAgeRestriction !== "number"
-) {
-  errors.errorsMessages.push({
-    message:
-      "min age restriction field should be a number or null if no age restriction",
-    field: "minAgeRestriction",
-  });
-}
+  if (
+    data.minAgeRestriction !== null &&
+    typeof data.minAgeRestriction !== "number"
+  ) {
+    errors.errorsMessages.push({
+      message:
+        "min age restriction field should be a number or null if no age restriction",
+      field: "minAgeRestriction",
+    });
+  }
 
   if (
     data.minAgeRestriction &&
@@ -72,12 +72,12 @@ if (
     });
   }
 
-   if (data.canBeDownloaded && typeof data.canBeDownloaded !== "boolean") {
-     errors.errorsMessages.push({
-       message: "can be downloaded field must be boolean type",
-       field: "canBeDownloaded",
-     });
-   }
+  if (data.canBeDownloaded && typeof data.canBeDownloaded !== "boolean") {
+    errors.errorsMessages.push({
+      message: "can be downloaded field must be boolean type",
+      field: "canBeDownloaded",
+    });
+  }
 
   const invalidResolutions =
     data.availableResolutions?.filter(
@@ -93,17 +93,16 @@ if (
     });
   }
 
-
-   if (data.publicationDate && typeof data.publicationDate !== "string") {
-     errors.errorsMessages.push({
-       message: "publication date field must be string type",
-       field: "publicationDate",
-     });
-   }
+  if (data.publicationDate && typeof data.publicationDate !== "string") {
+    errors.errorsMessages.push({
+      message: "publication date field must be string type",
+      field: "publicationDate",
+    });
+  }
 
   if (data.publicationDate) {
     const isoRegex =
-      /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/;
+      /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
     const publicationDateTest = isoRegex.test(data.publicationDate);
     if (!publicationDateTest) {
       errors.errorsMessages.push({
