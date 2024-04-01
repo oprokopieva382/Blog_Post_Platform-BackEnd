936fb501d21fc9c914fa02908408d2aa8cdfe578
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-01 14:17:22 -0500

    modify db for posts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,20 +1,21 @@
 export type DBType = {
-  videos: any[];
-  //users
+  //videos: any[];
+  posts: any[]
 };
 
 export const db: DBType = {
-  videos: [],
-  //users
+  //videos: [],
+  posts: []
 };
 
 export const setDB = (dataset?: Partial<DBType>) => {
   if (!dataset) {
-    db.videos = [];
-    //db.users
+    //db.videos = [];
+    db.posts = []
     return;
   }
 
-  db.videos = dataset.videos || db.videos
-  //db.users
+  //db.videos = dataset.videos || db.videos
+  db.posts = dataset.posts || db.posts;
+
 };
