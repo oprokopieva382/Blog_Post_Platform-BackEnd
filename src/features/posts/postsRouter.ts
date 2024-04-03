e1c7ff0046f306e775a5552bc53ce15d05b668ab
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-02 23:26:34 -0500

    create authMiddleware and set up on post request for create post

--- src/features/posts/postsRouter.ts
+++ src/features/posts/postsRouter.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { postsController } from "./postsController";
-
+import { authMiddleware } from "../../middlewares/authMiddleware";
 
 export const postsRouter = Router();
 
-
 postsRouter.get("/", postsController.getAll());
 postsRouter.get("/:id", postsController.getById());
-postsRouter.post("/", postsController.create());
+postsRouter.post("/", authMiddleware, postsController.create());
 postsRouter.delete("/:id", postsController.deleteById());
 postsRouter.put("/:id", postsController.update());
