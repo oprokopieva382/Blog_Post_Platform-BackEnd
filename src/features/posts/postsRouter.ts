7cd21f085f007fda7d323757c888f6212e588ca1
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-02 23:29:34 -0500

    set up auth for post put and delete requests of posts

--- src/features/posts/postsRouter.ts
+++ src/features/posts/postsRouter.ts
@@ -7,5 +7,5 @@ export const postsRouter = Router();
 postsRouter.get("/", postsController.getAll());
 postsRouter.get("/:id", postsController.getById());
 postsRouter.post("/", authMiddleware, postsController.create());
-postsRouter.delete("/:id", postsController.deleteById());
-postsRouter.put("/:id", postsController.update());
+postsRouter.delete("/:id", authMiddleware, postsController.deleteById());
+postsRouter.put("/:id", authMiddleware, postsController.update());
