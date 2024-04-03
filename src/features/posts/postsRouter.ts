02bb5aa5d738bd49fef5f505a61a24052d5df119
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-03 10:03:18 -0500

    create validation middleware with error display

--- src/features/posts/postsRouter.ts
+++ src/features/posts/postsRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { postsController } from "./postsController";
 import { authMiddleware } from "../../middlewares/authMiddleware";
+import { validationMiddleware } from './../../middlewares/validationMiddleware';
 
 export const postsRouter = Router();
 
 postsRouter.get("/", postsController.getAll());
 postsRouter.get("/:id", postsController.getById());
-postsRouter.post("/", authMiddleware, postsController.create());
+postsRouter.post("/", authMiddleware, validationMiddleware, postsController.create());
 postsRouter.delete("/:id", authMiddleware, postsController.deleteById());
-postsRouter.put("/:id", authMiddleware, postsController.update());
+postsRouter.put("/:id", authMiddleware, validationMiddleware, postsController.update());
---
+++ src/middlewares/validationMiddleware.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { postsRepository } from "../repositories/posts-repository";
+
+const validateBlogId = async (blogId: number) => {
+  const blog = await postsRepository.getByIdPost(blogId);
+  if (!blog) {
+    throw new Error("No blog exists with the provided ID");
+  }
+};
+
+export const validationMiddleware = [
+  body("title")
+    .trim()
+    .isString()
+    .withMessage("Title field must be a string")
+    .notEmpty()
+    .withMessage("Title field is required")
+    .isLength({ max: 30 })
+    .withMessage("max length of title 30 characters"),
+
+  body("shortDescription")
+    .trim()
+    .isString()
+    .withMessage("Description field must be a string")
+    .notEmpty()
+    .withMessage("Description field is required")
+    .isLength({ max: 100 })
+    .withMessage("max length of description 100 characters"),
+
+  body("content")
+    .trim()
+    .isString()
+    .withMessage("Content field must be a string")
+    .notEmpty()
+    .withMessage("Content field is required")
+    .isLength({ max: 1000 })
+    .withMessage("max length of content 100 characters"),
+
+  body("blogId").custom(async (blogId) => {
+    await validateBlogId(blogId);
+  }),
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    console.log(errors);
+    if (errors.length) {
+      return res.status(400).json({
+        errorsMessages: [{ message: errors[0].msg, field: errors[0].type }],
+      });
+    }
+    next();
+  },
+];
