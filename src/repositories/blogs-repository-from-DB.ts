d270df6f674dfc6c5f3d3eeeb042753824cc92a5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-10 12:49:17 -0500

    modify getAllBlogs so that all blogs view get id and it will be string instead of ObjectId

--- src/repositories/blogs-repository-from-DB.ts
+++ src/repositories/blogs-repository-from-DB.ts
@@ -1,13 +1,23 @@
 import { blogsCollection } from "../cloud_DB/mongo_db_atlas";
+import { BlogDBType } from "../cloud_DB/mongo_db_types";
+import { BlogViewModel } from "../models/BlogViewModel";
 
 export const blogsRepository = {
-  async getAllBlogs() {
-    try {
-      const blogs = await blogsCollection.find().toArray();
-      return blogs;
-    } catch (error) {
-      console.error("Error in fetching all blogs:", error);
-      throw error;
-    }
+  async getAllBlogs(): Promise<BlogViewModel[]> {
+    const blogs: BlogDBType[] = await blogsCollection.find().toArray();
+    const blogsToView: BlogViewModel[] = blogs.map(mapBlogDBToViewModel);
+    return blogsToView;
   },
+};
+
+export const mapBlogDBToViewModel = (blog: BlogDBType): BlogViewModel => {
+  return {
+    // Convert ObjectId to string
+    id: blog._id.toHexString(),
+    name: blog.name,
+    description: blog.description,
+    websiteUrl: blog.websiteUrl,
+    createdAt: blog.createdAt,
+    isMembership: blog.isMembership,
+  };
 };
