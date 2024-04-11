5720b741e11d56c8ad3a46ea6610efe93d93f438
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-11 13:57:59 -0500

    set up delete ALL MongoDB data from collections

--- src/features/dbCleanUp/testingRouter.ts
+++ src/features/dbCleanUp/testingRouter.ts
@@ -3,4 +3,6 @@ import { testingController } from "./testingController";
 
 export const testingRouter = Router();
 
-testingRouter.delete("/all-data", testingController);
+testingRouter.delete("/all-data", async (req, res) =>
+  await testingController(req, res)
+);
