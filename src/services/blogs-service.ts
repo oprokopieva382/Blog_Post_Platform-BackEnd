5ea20702de8e49caca4077503658bb08970963c5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-18 17:48:19 -0500

    fixed bug in  pagination

--- src/services/blogs-service.ts
+++ src/services/blogs-service.ts
@@ -26,7 +26,7 @@ export const blogsService = {
       : {};
 
     const blogs = await blogsRepository.getAllBlogs(
-      //search, 
+      search, 
       query);
     if (blogs.length === 0) {
       return null;
@@ -36,7 +36,7 @@ export const blogsService = {
 
     //prep blogs for output as Data Transfer Object
     const blogsToView = {
-      pagesCount: Math.ceil(totalBlogsCount / query.pageSize),
+      pagesCount: Math.ceil(totalBlogsCount -1 / query.pageSize),
       page: query.pageNumber,
       pageSize: query.pageSize,
       totalCount: totalBlogsCount,
@@ -96,7 +96,7 @@ export const blogsService = {
 
     //prep posts for output as Data Transfer Object
     const postsToView = {
-      pagesCount: Math.ceil(totalPostsCount / query.pageSize),
+      pagesCount: Math.ceil(totalPostsCount -1 / query.pageSize),
       page: query.pageNumber,
       pageSize: query.pageSize,
       totalCount: totalPostsCount,
