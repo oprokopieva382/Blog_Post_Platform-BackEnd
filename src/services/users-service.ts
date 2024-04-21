26968816d06c373707c8ef7757f538ecc305d3d0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-21 11:31:02 -0500

    add password hashing with bcrypt

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -1,16 +1,22 @@
 import { ObjectId } from "mongodb";
-import {UserInputModel,} from "../models";
+import { UserInputModel } from "../models";
 import { usersRepository } from "../repositories";
+import { bcryptService } from "./bcrypt-service";
 
 export const usersService = {
   async createUser(data: UserInputModel) {
-    const {login, password, email}= data
-    
+    const { login, password, email } = data;
+
+    const hashedPassword = bcryptService.createHash(password);
+
     const newUser = {
       _id: new ObjectId(),
+      login,
+      password: hashedPassword,
+      email,
       createdAt: new Date().toISOString(),
-      ...data,
     };
+    
     const createdUser = await usersRepository.createUser(newUser);
     const insertedId = createdUser.insertedId;
 
