8846a5594db339216c81f56d83492620a35e2593
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-04-22 13:15:41 -0500

    create auth router and controller

---
+++ src/features/auth/authController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export const authController = {
+  login: async (req: Request, res: Response) => {
+    try {
+    } catch (error) {
+      //   console.error("Error in fetching all users:", error);
+      //   res.status(500).json({ error: "Internal server error" });
+    }
+  },
+};
