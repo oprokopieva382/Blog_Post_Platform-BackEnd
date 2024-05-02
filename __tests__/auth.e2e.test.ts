a7a014cadc031652a7b3168e8ede2a3793559e9c
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-02 11:16:36 -0500

    finish auth tests

--- __tests__/auth.e2e.test.ts
+++ __tests__/auth.e2e.test.ts
@@ -2,6 +2,7 @@ import { ConnectMongoDB } from "../src/cloud_DB";
 import request from "supertest";
 import { app } from "../src/app";
 import { SETTINGS } from "../src/settings";
+import { authManager } from "../src/testManager";
 
 describe("auth tests", () => {
   beforeAll(async () => {
@@ -9,6 +10,8 @@ describe("auth tests", () => {
   });
 
   afterAll(async () => {});
+
+  let token: string;
 
   describe("AUTH LOGIN", () => {
     it("shouldn't login user and return status code 401 if password or login is wrong", async () => {
@@ -24,19 +27,42 @@ describe("auth tests", () => {
     });
 
     it("shouldn't login user and return status code 400 if incorrect values", async () => {
-      const dataWittLongPassword = {
+      const dataWithLongPassword = {
         loginOrEmail: "Tina@gmail.com",
         password: "string123hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
       };
 
       const res = await request(app)
         .post(`${SETTINGS.PATH.AUTH}/login`)
-        .send(dataWittLongPassword)
+        .send(dataWithLongPassword)
         .expect(400);
     });
 
-    // it("should login user and return JWT token with status code 200", async () => {
+    it("should login user and return status code 200", async () => {
+      const user = await authManager.loginUser();
 
-    // });
+      const res = await request(app)
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send(user)
+        .expect(200);
+
+      token = res.body.accessToken;
+    });
+  });
+
+  describe("AUTH ME", () => {
+    it("should auth me return status code 200 and object", async () => {
+      const res = await request(app)
+        .get(`${SETTINGS.PATH.AUTH}/me`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200);
+    });
+
+    it("shouldn't auth me and return status code 401", async () => {
+      const res = await request(app)
+        .get(`${SETTINGS.PATH.AUTH}/me`)
+        .set("Authorization", `Bearer ${token}+1`)
+        .expect(401);
+    });
   });
 });
--- src/testManager/authManager.ts
+++ src/testManager/authManager.ts
@@ -6,4 +6,6 @@ export const authManager = {
     };
     return loginInput;
   },
+
+  
 };
