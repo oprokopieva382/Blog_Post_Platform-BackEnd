9fe4c17eadad4f7cf022d4b0b03ae12ac6680225
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-08 17:43:34 -0500

    try to modify email-adapter, back to check again

--- src/features/auth/authRouter.ts
+++ src/features/auth/authRouter.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
 import { authController } from "./authController";
-import { authMiddleware, codeValidationMiddleware, emailValidationMiddleware, loginValidationMiddleware, userValidationMiddleware } from "../../middlewares";
+import {
+  authMiddleware,
+  codeValidationMiddleware,
+  emailValidationMiddleware,
+  loginValidationMiddleware,
+  userValidationMiddleware,
+} from "../../middlewares";
 
 export const authRouter = Router();
 
 authRouter.post("/login", loginValidationMiddleware, authController.login);
 authRouter.get("/me", authMiddleware, authController.me);
-authRouter.post("/registration", userValidationMiddleware, authController.registration);
+authRouter.post(
+  "/registration",
+  userValidationMiddleware,
+  authController.registration
+);
 authRouter.post(
   "/registration-confirmation",
-  codeValidationMiddleware, authController.registrationConfirmation
+  codeValidationMiddleware,
+  authController.registrationConfirmation
 );
 authRouter.post(
   "/registration-email-resending",
