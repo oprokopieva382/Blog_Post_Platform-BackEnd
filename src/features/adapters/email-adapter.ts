ba48f0c7ad58b87a08011939808d353cb9bae56e
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-08 17:29:13 -0500

    try to modify email-adapter, back to check again

--- src/features/adapters/email-adapter.ts
+++ src/features/adapters/email-adapter.ts
@@ -17,7 +17,7 @@ export const emailAdapter = {
       subject: "Email confirmation",
       html: `<h1>Thank you for your registration</h1>
       <p>To finish registration, please follow the link below:</p>
-      <p><a href="https://stackoverflow.com/confirm-email?code=${code}">Complete registration</a></p>`,
+      <p><a href="https://somesite.com/confirm-email?code=${code}">Complete registration</a></p>`,
     });
 
     return !!emailInfo;
