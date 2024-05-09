c6936b8a90c753db175368a25f37517a239dde19
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-05-09 09:28:52 -0500

    remove no needed files

--- src/middlewares/postValidationMiddleware.ts
+++ src/middlewares/postValidationMiddleware.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { FieldValidationError, body, validationResult } from "express-validator";
+import {
+  FieldValidationError,
+  body,
+  validationResult,
+} from "express-validator";
 import { blogsRepository } from "../repositories/blogs-repository";
 
 const validateBlogId = async (blogId: string) => {
@@ -17,60 +21,52 @@ export const postValidationMiddleware = async (
 ) => {
   const allBodyValidation: any[] = [];
 
-  
-    allBodyValidation.push(
-      body("title")
-        .trim()
-        .isString()
-        .withMessage("Title field must be a string")
-        .notEmpty()
-        .withMessage("Title field is required")
-        .isLength({ max: 30 })
-        .withMessage("max length of title 30 characters")
-    );
-  
+  allBodyValidation.push(
+    body("title")
+      .trim()
+      .isString()
+      .withMessage("Title field must be a string")
+      .notEmpty()
+      .withMessage("Title field is required")
+      .isLength({ max: 30 })
+      .withMessage("max length of title 30 characters")
+  );
 
+  allBodyValidation.push(
+    body("shortDescription")
+      .trim()
+      .isString()
+      .withMessage("Description field must be a string")
+      .notEmpty()
+      .withMessage("Description field is required")
+      .isLength({ max: 100 })
+      .withMessage("max length of description 100 characters")
+  );
 
-    allBodyValidation.push(
-      body("shortDescription")
-        .trim()
-        .isString()
-        .withMessage("Description field must be a string")
-        .notEmpty()
-        .withMessage("Description field is required")
-        .isLength({ max: 100 })
-        .withMessage("max length of description 100 characters")
-    );
-  
+  allBodyValidation.push(
+    body("content")
+      .trim()
+      .isString()
+      .withMessage("Content field must be a string")
+      .notEmpty()
+      .withMessage("Content field is required")
+      .isLength({ max: 1000 })
+      .withMessage("max length of content 1000 characters")
+  );
 
-
-    allBodyValidation.push(
-      body("content")
-        .trim()
-        .isString()
-        .withMessage("Content field must be a string")
-        .notEmpty()
-        .withMessage("Content field is required")
-        .isLength({ max: 1000 })
-        .withMessage("max length of content 1000 characters")
-    );
-  
-
-
-    allBodyValidation.push(
-      body("blogId")
-        .trim()
-        .notEmpty()
-        .withMessage("blogId field is required")
-        .custom(async (blogId) => {
-          await validateBlogId(blogId);
-        })
-    );
-  
+  allBodyValidation.push(
+    body("blogId")
+      .trim()
+      .notEmpty()
+      .withMessage("blogId field is required")
+      .custom(async (blogId) => {
+        await validateBlogId(blogId);
+      })
+  );
 
   await Promise.all(allBodyValidation.map((item) => item.run(req)));
 
-  const errors = validationResult(req)
+  const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     //console.log(errors.array());
@@ -80,7 +76,7 @@ export const postValidationMiddleware = async (
       onlyFirstError: true,
     }) as FieldValidationError[];
     return res.status(400).json({
-        errorsMessages: errorsFields.map((error) => ({
+      errorsMessages: errorsFields.map((error) => ({
         message: error.msg,
         field: error.path,
       })),
