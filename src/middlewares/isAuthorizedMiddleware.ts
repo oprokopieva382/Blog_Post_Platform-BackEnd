5a96dd8a6dc3de8db3c02d277585901ec5f54620
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-20 00:48:29 -0500

    add redis to the project to cash userId for 1 hour in isAuthorizedMiddleware

--- src/middlewares/isAuthorizedMiddleware.ts
+++ src/middlewares/isAuthorizedMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { usersQueryRepository } from "../query_repositories";
 import { ApiError } from "../helper/api-errors";
 import { jwtService } from "../features/application";
+import redisClient from "../redisClient";
 
 export const isAuthorizedMiddleware = async (
   req: Request,
@@ -18,21 +19,32 @@ export const isAuthorizedMiddleware = async (
     const token = req.headers.authorization.split(" ")[1];
 
     const userId = await jwtService.getUserIdByAccessToken(token);
-  
+
     if (!userId) {
       throw ApiError.UnauthorizedError("Not authorized", [
         "Authorization failed. Access token is incorrect or expired",
       ]);
     }
 
-    const foundUser = await usersQueryRepository.getByIdUser(userId);
-    if (!foundUser) {
-      throw ApiError.UnauthorizedError("Not authorized", [
-        "Authorization failed. Can't find user with such id",
-      ]);
+    let authorizedUser;
+    const cashedUser = await redisClient.get(`user:${userId}`);
+
+    if (!cashedUser) {
+      authorizedUser = await usersQueryRepository.getByIdUser(userId);
+      if (!authorizedUser) {
+        throw ApiError.UnauthorizedError("Not authorized", [
+          "Authorization failed. Can't find user with such id",
+        ]);
+      }
+
+      await redisClient.set(`user:${userId}`, JSON.stringify(authorizedUser), {
+        EX: 3600, // Cache for 1 hour
+      });
+    } else {
+      authorizedUser = JSON.parse(cashedUser);
     }
 
-    req.user = foundUser;
+    req.user = authorizedUser;
     next();
   } catch (error) {
     next(error);
