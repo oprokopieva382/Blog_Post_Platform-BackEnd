5b855e9af5811362da672daa51cb54bb02d9b089
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-27 14:02:57 -0500

    commentsController transition to transactions script class

--- src/features/comments/commentsRouter.ts
+++ src/features/comments/commentsRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { commentsController } from "./commentsController";
+import { commentsController } from "./CommentsController";
 import { isAuthorizedMiddleware, validateComment } from "../../middlewares";
 
 export const commentsRouter = Router();
