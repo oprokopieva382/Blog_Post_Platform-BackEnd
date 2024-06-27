56a6c1400574d2aded0c01eb18597d8d8c8989bc
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-06-27 14:08:53 -0500

    postsController transition to transactions script class

--- src/features/posts/postsRouter.ts
+++ src/features/posts/postsRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postsController } from "./postsController";
+import { postsController } from "./PostsController";
 import {
   isAdminMiddleware,
   isAuthorizedMiddleware,
