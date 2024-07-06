84007bb2b5c2cd9fcac858ad24827b13d641eb55
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-07-06 18:11:40 -0500

    code refactor in EmailService

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 import { SETTINGS } from "../settings";
 
-export class EmailService  {
-  async sendEmail(email: string, code: string) {
+export class EmailService {
+  private async sendEmail(to: string, subject: string, html: string) {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -11,40 +11,34 @@ export class EmailService  {
       },
     });
 
-    const emailInfo = transporter.sendMail({
-      from: `"BlogPosts Registration" <${SETTINGS.REGISTRATION_EMAIL}>`,
-      to: email,
-      subject: "Email confirmation",
-      html: `
-        <h1>Thank you for your registration</h1>
-        <p>To finish registration please follow the link below:
-        <a href="https://google.com/confirm-email?code=${code}">complete registration</a></p>
-      `,
+    const emailInfo = await transporter.sendMail({
+      from: `"BlogPosts" <${SETTINGS.REGISTRATION_EMAIL}>`,
+      to,
+      subject,
+      html,
     });
 
+    console.log("Email sent: ", emailInfo.response);
     return !!emailInfo;
   }
-  
-  async passwordRecovery(email: string, code: string) {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: SETTINGS.REGISTRATION_EMAIL,
-        pass: SETTINGS.REGISTRATION_PASS,
-      },
-    });
 
-    const emailInfo = transporter.sendMail({
-      from: `"BlogPosts Password Recovery" <${SETTINGS.REGISTRATION_EMAIL}>`,
-      to: email,
-      subject: "Password recovery",
-      html: `
-        <h1>Password recovery</h1>
-        <p>To finish password recovery please follow the link below:
-        <a href="https://google.com/password-recovery?recoveryCode=${code}">This link will expire in one hour.</a></p>
-      `,
-    });
-
-    return !!emailInfo;
+  async sendRegistrationEmail(email: string, code: string) {
+    const subject = "Email confirmation";
+    const html = `
+      <h1>Thank you for your registration</h1>
+      <p>To finish registration please follow the link below:
+      <a href="https://google.com/confirm-email?code=${code}">complete registration</a></p>
+    `;
+    return this.sendEmail(email, subject, html);
   }
-};
+
+  async sendPasswordRecoveryEmail(email: string, code: string) {
+    const subject = "Password recovery";
+    const html = `
+      <h1>Password recovery</h1>
+      <p>To finish password recovery please follow the link below:
+      <a href="https://google.com/password-recovery?recoveryCode=${code}">This link will expire in one hour.</a></p>
+    `;
+    return this.sendEmail(email, subject, html);
+  }
+}
